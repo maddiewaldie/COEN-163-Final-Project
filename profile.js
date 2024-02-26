a28a882d63759c5d09483648ff6ce45f13427339
Romeo Nickel <106633433+Romeo-5@users.noreply.github.com>
2024-02-26 11:23:01 -0800

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,6 @@
+const prefButton = document.querySelector('#preferences');
+
+// Go to preferences page when prefButton clicked
+prefButton.addEventListener('click', function (e) {
+    window.location.href = "preferences.html";
+}); 
