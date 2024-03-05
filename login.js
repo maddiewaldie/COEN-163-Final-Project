49b5ef4c3374bc0d388ece94d1bec4dd96a7c3f8
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2024-03-04 16:06:20 -0800

    upd

--- login.js
+++ login.js
@@ -1,26 +1,21 @@
-const form = document.querySelector("form");
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-});
-
 const togglePassword = document.querySelector("#togglePassword");
-        const password = document.querySelector("#password");
-togglePassword.addEventListener('click', function (e) { 
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password'; // toggle the type attribute
-    password.setAttribute('type', type);
-    this.classList.toggle('bi-eye'); // toggle the eye/eye slash icon
+const password = document.querySelector("#password");
+togglePassword.addEventListener("click", function(e) {
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password"; // toggle the type attribute
+  password.setAttribute("type", type);
+  this.classList.toggle("bi-eye"); // toggle the eye/eye slash icon
 });
-
 
 // Get the modal
-var modal = document.getElementById('create');
+var modal = document.getElementById("create");
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
 //Password requirements
 var myInput = document.getElementById("newPW");
@@ -32,28 +27,28 @@ var length = document.getElementById("length");
 // When the user clicks on the password field, show the message box
 myInput.onfocus = function() {
   document.getElementById("message").style.display = "block";
-}
+};
 
 // When the user clicks outside of the password field, hide the message box
 myInput.onblur = function() {
   document.getElementById("message").style.display = "none";
-}
+};
 
 // When the user starts to type something inside the password field
 myInput.onkeyup = function() {
   // Validate lowercase letters
   var lowerCaseLetters = /[a-z]/g;
-  if(myInput.value.match(lowerCaseLetters)) {  
+  if (myInput.value.match(lowerCaseLetters)) {
     letter.classList.remove("invalid");
     letter.classList.add("valid");
   } else {
     letter.classList.remove("valid");
     letter.classList.add("invalid");
   }
-  
+
   // Validate capital letters
   var upperCaseLetters = /[A-Z]/g;
-  if(myInput.value.match(upperCaseLetters)) {  
+  if (myInput.value.match(upperCaseLetters)) {
     capital.classList.remove("invalid");
     capital.classList.add("valid");
   } else {
@@ -63,48 +58,49 @@ myInput.onkeyup = function() {
 
   // Validate numbers
   var numbers = /[0-9]/g;
-  if(myInput.value.match(numbers)) {  
+  if (myInput.value.match(numbers)) {
     number.classList.remove("invalid");
     number.classList.add("valid");
   } else {
     number.classList.remove("valid");
     number.classList.add("invalid");
   }
-  
+
   // Validate length
-  if(myInput.value.length >= 8) {
+  if (myInput.value.length >= 8) {
     length.classList.remove("invalid");
     length.classList.add("valid");
   } else {
     length.classList.remove("valid");
     length.classList.add("invalid");
   }
-}
+};
 
 function navBar() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
 
-  //create account, submit button
-  function ValRedir(event) {
-    event.preventDefault();
-    let uname = document.getElementById("newUname").value;
-    let newpw = document.getElementById("newPW").value;
-    let repeatpw = document.getElementById("psw-repeat").value;
+//create account, submit button
+function ValRedir(event) {
+  event.preventDefault();
+  let uname = document.getElementById("newUname").value;
+  let newpw = document.getElementById("newPW").value;
+  let repeatpw = document.getElementById("psw-repeat").value;
 
-    if(uname=="" || newpw=="" || repeatpw==""){
-      alert("All fields must be filled out");
-      return false;
-    }
-    if (newpw != repeatpw) {
-      alert("Passwords must match");
-      return false;
-    }
-    //Need to add more validation requirements
-    return true;
+  if (uname == "" || newpw == "" || repeatpw == "") {
+    alert("All fields must be filled out");
+    return false;
   }
+  if (newpw != repeatpw) {
+    alert("Passwords must match");
+    return false;
+  }
+  //Need to add more validation requirements
+  console.log("login");
+  return true;
+}
